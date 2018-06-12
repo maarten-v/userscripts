5d7ff240a01d0457e08d9f2525c14f88ef86e6ad
Maarten Vliegenthart <maarten@maximum.nl>
2018-06-12 17:07:23 +0200

    typo

--- Nu.nl remove header.js
+++ Nu.nl remove header.js
@@ -5,7 +5,7 @@
 // @include     https://www.nu*.nl/*
 // @version     1.0.0
 // @namespace      https://github.com/maarten-v/userscripts/
-// @updateURL      https://github.com/maarten-v/userscripts/raw/master/nu.nl%20remove%20header.js
+// @updateURL      https://github.com/maarten-v/userscripts/raw/master/Nu.nl%20remove%20header.js
 // @author         Maarten Vliegenthart
 // @description Removes useless pageheader from top of screen
 // ==/UserScript==
