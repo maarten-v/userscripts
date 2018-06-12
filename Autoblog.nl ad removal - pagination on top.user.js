6d7a6e530d3a500f19f458b55b2b7d396df3b33c
Maarten Vliegenthart <maarten@maximum.nl>
2018-06-12 17:04:22 +0200

    update url

--- Autoblog.nl ad removal - pagination on top.user.js
+++ Autoblog.nl ad removal - pagination on top.user.js
@@ -4,7 +4,7 @@
 // @grant          none
 // @version        1.0.0
 // @namespace      https://github.com/maarten-v/userscripts/
-// @updateURL      https://github.com/maarten-v/userscripts/
+// @updateURL      https://github.com/maarten-v/userscripts/raw/master/Autoblog.nl%20ad%20removal%20-%20pagination%20on%20top.user.js
 // @author         Maarten Vliegenthart
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js
 // ==/UserScript==
