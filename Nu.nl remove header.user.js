6cc0504553d2a31f4900a414059ff40218846735
Maarten <github@vliegenthart.org>
2019-04-04 14:31:22 +0200

    different header

--- Nu.nl remove header.user.js
+++ Nu.nl remove header.user.js
@@ -3,16 +3,14 @@
 // @namespace   https://github.com/maarten-v/userscripts/
 // @include     https://nu*.nl/*
 // @include     https://www.nu*.nl/*
-// @version     1.0.0
+// @version     1.0.1
 // @namespace      https://github.com/maarten-v/userscripts/
 // @updateURL      https://github.com/maarten-v/userscripts/raw/master/Nu.nl%20remove%20header.js
 // @author         Maarten Vliegenthart
 // @description Removes useless pageheader from top of screen
 // ==/UserScript==
 
-let HeaderFoetsie = document.getElementsByClassName('webring');
+let HeaderFoetsie = document.getElementsByClassName('topmenu');
 while (HeaderFoetsie.length) {
     HeaderFoetsie[0].parentNode.removeChild(HeaderFoetsie[0]);
 }
-
-document.getElementById('header').remove();
