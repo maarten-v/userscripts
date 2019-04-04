b7d84fe7b8cd0daef4fcbba6237a006d87868505
Maarten <github@vliegenthart.org>
2019-04-04 14:41:41 +0200

    updateurl

--- Buienradar header and ad removal.user.js
+++ Buienradar header and ad removal.user.js
@@ -4,6 +4,7 @@
 // @grant          none
 // @version        1.0.0
 // @namespace      https://github.com/maarten-v/userscripts/
+// @updateURL      https://github.com/maarten-v/userscripts/raw/master/Buienradar%20header%20and%20ad%20removal.user.js
 // @author         Maarten Vliegenthart
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js
 // ==/UserScript==
