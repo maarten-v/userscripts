2fcf72772b83beb58476854696d3cfc8ad0b7d24
Maarten <github@vliegenthart.org>
2020-01-30 22:23:33 +0100

    Update Autoblog.nl ad removal - pagination on top.user.js

--- Autoblog.nl ad removal - pagination on top.user.js
+++ Autoblog.nl ad removal - pagination on top.user.js
@@ -2,7 +2,7 @@
 // @name           Autoblog.nl ad removal / pagination on top
 // @include        https://www.autoblog.nl/*
 // @grant          none
-// @version        1.0.5
+// @version        1.0.6
 // @namespace      https://github.com/maarten-v/userscripts/
 // @updateURL      https://github.com/maarten-v/userscripts/raw/master/Autoblog.nl%20ad%20removal%20-%20pagination%20on%20top.user.js
 // @author         Maarten Vliegenthart
@@ -12,10 +12,11 @@
 // fade branded content
 $('p.entry-meta:contains("Branded content")').parents('article').css('opacity',0.5)
 
+//remove sidebar
 $('aside').remove();
 
 // remove sitepicker
 document.getElementById('sitepicker').remove();
 
-// copy navigation to top
-$('.ab_navigation').clone().prependTo($('.ab_content_wrapper').first())
+// copy pagination to top
+$('div.pagination').clone().prependTo($('main'))
