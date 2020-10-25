6cf63b11820892586a2a3b21fb579126f6b58131
Maarten <github@vliegenthart.org>
2020-10-25 22:45:10 +0100

    Update Autoblog.nl ad removal - pagination on top.user.js
    update jquery / add advertorial blocking

--- Autoblog.nl ad removal - pagination on top.user.js
+++ Autoblog.nl ad removal - pagination on top.user.js
@@ -6,11 +6,11 @@
 // @namespace      https://github.com/maarten-v/userscripts/
 // @updateURL      https://github.com/maarten-v/userscripts/raw/master/Autoblog.nl%20ad%20removal%20-%20pagination%20on%20top.user.js
 // @author         Maarten Vliegenthart
-// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js
+// @require        https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // ==/UserScript==
 
 // fade branded content
-$('p.entry-meta:contains("Branded content")').parents('article').css('opacity',0.5)
+$('p.entry-meta:contains("advertorial"),p.entry-meta:contains("Branded content")').parents('article').css('opacity',0.5)
 
 //remove sidebar
 $('aside').remove();
